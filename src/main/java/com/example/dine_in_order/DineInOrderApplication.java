7b3d271cfd96da1cf124284084fae22b0b0ff39c
Vinay8555 <vinayswagukar@gmail.com>
2025-02-19 16:46:41 +0530

    adding repository

---
+++ src/main/java/com/example/dine_in_order/DineInOrderApplication.java
@@ -0,0 +1,13 @@
+package com.example.dine_in_order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DineInOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DineInOrderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dine_in_order/DineInOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dine_in_order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DineInOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
